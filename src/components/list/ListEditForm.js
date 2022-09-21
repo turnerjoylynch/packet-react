2825455f2cb142db4b39261b7c6e943c412fec48
turner lynch <turnerjoylynch@gmail.com>
2022-09-20 20:21:59 -0700

    MVP

--- src/components/list/ListEditForm.js
+++ src/components/list/ListEditForm.js
@@ -1,72 +1,83 @@
 import React, { useState, useEffect } from "react"
-import {useHistory, useParams} from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { updateList, deleteList, getListById } from "../../modules/ListManager.js";
 
 export const ListEditForm = () => {
-    const history = useHistory();
-    const {listId} = useParams();
-    // const [isLoading, setIsLoading] = useState(true);
+  const history = useHistory();
+  const { listId } = useParams();
 
-    const [currentList, setCurrentList] = useState({ list_name: ""});
-
-    const loadList = () => {
-        if (listId) {
-            getListById(listId)
-                .then(data => {
-                    setCurrentList({
-                        id: listId,
-                        list_name: data.list_name
-                    })
-            })
-        }
-        
-    }
-    
-    useEffect(() => {
-        loadList(listId)
-    }, [listId])
+  const [currentList, setCurrentList] = useState({
+    id: 0,
+    list_name: "",
+    created_on: ""
+  });
 
 
-    const handleFieldChange = (domEvent) => {
-        const updatedList = {...currentList}
-        let selectedVal = domEvent.target.value
-        updatedList[domEvent.target.name] = selectedVal
-        setCurrentList(updatedList)
+  const loadList = () => {
+    if (listId) {
+      getListById(listId)
+        .then(data => {
+          setCurrentList({
+            id: parseInt(listId),
+            list_name: data.list_name,
+            created_on: data.created_on
+          })
+        })
     }
 
-    return (
-        <>
-          <form>
-            <fieldset>
-              <div className="formgrid">
-                <input
-                  type="text"
-                  required
-                  className="form-control"
-                  id="list_name"
-                  value={currentList.list_name}
-                  onChange={handleFieldChange}
-                />
-                <label htmlFor="list_name"> List name</label>
-              </div>
-              <button type="submit"
-                onClick={p => {
-                    p.preventDefault()
+  }
 
-                    const editedList = {
-                        id: listId,
-                        list_name: currentList.list_name
-                    }
-                    
-                    updateList(editedList, listId)
-                        .then(() => history.push('/list'))
-                }}
-                className="btn btn-primary" 
-                id="createBtn">Update</button>
+  useEffect(() => {
+    loadList(listId)
+  }, [listId])
 
-            <button className="btn btn-danger" onClick={() => {deleteList(listId)}}>Delete</button>
-            </fieldset>
-          </form>
-        </>
-      );
-    }
+
+  const handleFieldChange = (domEvent) => {
+    const updatedList = { ...currentList }
+    let selectedVal = domEvent.target.value
+    updatedList[domEvent.target.name] = selectedVal
+    setCurrentList(updatedList)
+  }
+
+  return (
+    <>
+      <form>
+        <fieldset>
+          <div className="formgrid">
+            <label htmlFor="list_name"> List name</label>
+            <input
+              type="text"
+              required
+              autoFocus
+              className="form-control"
+              name="list_name"
+              value={currentList.list_name}
+              onChange={handleFieldChange}
+            />
+
+          </div>
+        </fieldset>
+        <button type="submit"
+          onClick={p => {
+            p.preventDefault()
+
+            const editedList = {
+              id: listId,
+              list_name: currentList.list_name,
+              created_on: currentList.created_on
+            }
+
+            updateList(editedList)
+              .then(() => history.push('/list'))
+          }}
+          className="btn btn-primary"
+          id="createBtn">Update</button>
+
+        <button className="btn btn-danger" onClick={(p) => { 
+          p.preventDefault() 
+          deleteList(listId)
+          .then(() => history.push('/list')) }}>Delete</button>
+      </form>
+    </>
+  );
+}
